487a4929a2f2bc9b1219c8f6684944dbb8ea6839
Nícolas Meira da Silva Schirmer <nschirmer.dev@gmail.com>
2018-02-20 05:59:12 -0300

    timer fragment layout

--- PomodoroProject/app/src/main/java/com/nschirmer/pomodoro/MainActivity.java
+++ PomodoroProject/app/src/main/java/com/nschirmer/pomodoro/MainActivity.java
@@ -3,39 +3,49 @@ package com.nschirmer.pomodoro;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.bottommenu_timer:
-                    mTextMessage.setText(R.string.bottommenu_title_timer);
-                    return true;
-                case R.id.bottommenu_history:
-                    mTextMessage.setText(R.string.bottommenu_title_history);
-                    return true;
-            }
-            return false;
-        }
-    };
+    private Fragment fragmentOpenned;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        fragmentOpenned = new TimerFragment();
+        startFragment(fragmentOpenned, "timer");
+    }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.bottommenu_timer:
+                    startFragment(new TimerFragment(), "timer");
+                    return true;
+                case R.id.bottommenu_history:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+
+    private void startFragment(Fragment fragment, String fragmentTag){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_fragment_container, fragment, fragmentTag);
+        fragmentTransaction.commitAllowingStateLoss();
     }
 
 }
---
+++ PomodoroProject/app/src/main/java/com/nschirmer/pomodoro/TimerFragment.java
@@ -0,0 +1,24 @@
+package com.nschirmer.pomodoro;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TimerFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_timer, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+}
